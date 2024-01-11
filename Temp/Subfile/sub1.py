e78847619819261bb019921e71f3346fbb8abf71
naveen-thangaraj <101919006+naveen-thangaraj@users.noreply.github.com>
2024-01-11 10:17:51 +0530

    Add files via upload

---
+++ Temp/Subfile/sub1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 11 10:13:07 2024
+
+@author: naveen
+"""
+
+class main1:
+    def fun1():
+        print('subfile call')
---
+++ Temp/main.py
@@ -0,0 +1,5 @@
+from Subfile.sub1 import main1
+
+step1=main1.fun1()
+
+print('Welcome!')
