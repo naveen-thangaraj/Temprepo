aaa9f8e08653e80aec36b763945e21b31a17bcd4
naveen-thangaraj <101919006+naveen-thangaraj@users.noreply.github.com>
2024-01-23 12:54:23 +0530

    Add files via upload

---
+++ Claims_Ingestion.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 26 13:24:51 2023
+@author: Naveen Thangaraj
+"""
+
+# Package import
+
+import pandas as pd
+import boto3
+import mysql.connector
+
+
+# Set the connection details
+connection_config = {
+    'user': 'cholams',
+    'password': 'Sunshine@321#',
+    'host': '10.107.48.42',
+    'database': 'cholams',
+    'port': '3306'
+}
+
+# Establish a connection to the database
+connection = mysql.connector.connect(**connection_config)
+
+# Check the connection status
+#if connection_string:
+if connection.is_connected():
+    
+    print("Connection successfully established")
+    
+else:
+    print('Connection to the host failed')
