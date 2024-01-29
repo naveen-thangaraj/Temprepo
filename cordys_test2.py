e6d47cafedaf5e9403a5338641e20ce02b38d658
naveen-thangaraj <101919006+naveen-thangaraj@users.noreply.github.com>
2024-01-29 18:30:26 +0530

    Update cordys_test2.py

--- cordys_test2.py
+++ cordys_test2.py
@@ -1,15 +1,9 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jan 29 17:47:36 2024
-
-@author: naveen
-"""
-
 import cx_Oracle
  
-con = cx_Oracle.connect('ins/ins620@10.105.24.10:1521/ggpoc')
-
-if con:
+# Connect as user "hr" with password "welcome" to the "orclpdb1" service running on this computer.
+connection = cx_Oracle.connect(user="ins", password="ins620",
+                               dsn="10.105.24.10:1521/ggpoc")
+if connection:
    print('Connection Establish') 
 else:
    print('Connection not working')
