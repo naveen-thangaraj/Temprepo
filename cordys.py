eb41b421e25137634df1e00fa798da6ae43c631a
naveen-thangaraj <101919006+naveen-thangaraj@users.noreply.github.com>
2024-01-29 12:40:06 +0530

    Update cordys.py

--- cordys.py
+++ cordys.py
@@ -1,7 +1,10 @@
 import oracledb
 
-connection = oracledb.connect(user="ins", password="ins610",
-                              dsn=" 10.105.24.18:1521/orclpdb")
+# connection = oracledb.connect(user="ins", password="ins610",
+#                               dsn="10.105.24.18:1521/orclpdb")
+
+# Increase the timeout to 60 seconds
+connection = oracledb.connect(user='ins', password='ins620', dsn='10.105.24.18:1521/ggpoc', timeout=60)
 
 if connection:
     print('connection sucessfull')
